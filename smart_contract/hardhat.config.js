d722cc2419b9305ef910e2ecef0a5b0a08fbb219
DevDuck-JS <103818112@student.swin.edu.au>
2024-10-24 18:37:19 +1100

    Build: Set up Solidity * install ethers, hardhat and other dependencies * create deploy.js script * deployed smart contract via sepolia

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-sepolia.g.alchemy.com/v2/enzf7hOs3Up_tFHoT2YKMQNgBCqyhgB1
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { solidity } = require("ethereum-waffle");
+
+require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
-  solidity: "0.8.27",
+  solidity: "0.8.0",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/enzf7hOs3Up_tFHoT2YKMQNgBCqyhgB1",
+      accounts: [
+        "0af9f98c65e0baf7f7062e67e41b4b83de112fa02746005e0c5fe7545f539faa",
+      ],
+    },
+  },
 };
